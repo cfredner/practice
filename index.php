e4e48b1499c849c10971d09b7896f291d3726320
unknown <cayemifr@2B204-MJRWFYY.2003moonlight.local>
2013-10-21 16:06:25 -0400

    testing

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Midterm Practice</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id"nav">
+			<ul>
+				<li>Home</a></li>
+				<li>Products</li>
+				<li>Services</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</div>
+		<h1><img src="logo.png" class="small"/> Evolution of the Bicycle</h1>
+		
+		
+		<div class="colum1of3">
+			<h3>The First Bycicle</h3>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div>
+		
+		
+		<div class="colum2of3">
+			<h3>The First Bycicle</h3>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div>
+		
+		
+		
+	</body>
+</html>
+
